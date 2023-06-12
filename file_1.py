96a5b5bfd7e68914173290f05d11838d4ebd4047
TsukasaMiyaji <tsukasakurvana@g.ucla.edu>
2023-06-12 13:31:31 -0700

    initial commit

---
+++ file_1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+A = 4
+b = 7
+
+print(A*b)
+
---
+++ file_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import cmath
+
+
+# In[5]:
+
+
+cmath.sqrt(14586)
+
